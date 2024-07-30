efb6bf9b6118d36ffe723c269e55e6a8cf54ee5d
SomyaPadhy4501 <73406675+SomyaPadhy4501@users.noreply.github.com>
2024-07-30 15:57:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAAufX1z6AgiVsTKXfiV_mCD85dA3Cmlw0",
-    authDomain: "home-app-669ba.firebaseapp.com",
-    projectId: "home-app-669ba",
-    storageBucket: "home-app-669ba.appspot.com",
-    messagingSenderId: "983310107946",
-    appId: "1:983310107946:web:3cd486f744383eb30aca53"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   export const app = initializeApp(firebaseConfig);
