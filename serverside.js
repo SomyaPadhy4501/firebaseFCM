68b116414898a150986367c2cf95413fa0e32468
SomyaPadhy4501 <73406675+SomyaPadhy4501@users.noreply.github.com>
2024-07-30 15:56:01 +0530

    Update serverside.js

--- serverside.js
+++ serverside.js
@@ -4,12 +4,12 @@ const admin = require('firebase-admin');
 const app = express();
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAAufX1z6AgiVsTKXfiV_mCD85dA3Cmlw0",
-    authDomain: "home-app-669ba.firebaseapp.com",
-    projectId: "home-app-669ba",
-    storageBucket: "home-app-669ba.appspot.com",
-    messagingSenderId: "983310107946",
-    appId: "1:983310107946:web:3cd486f744383eb30aca53"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 admin.initializeApp({
